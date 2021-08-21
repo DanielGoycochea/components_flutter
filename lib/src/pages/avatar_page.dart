983544cad2c4604d9fe106082b96353f364b54b0
Daniel Goycochea <danielgoycocheardz2gmail.com>
2021-08-21 17:39:07 -0500

    add avatar

--- lib/src/pages/avatar_page.dart
+++ lib/src/pages/avatar_page.dart
@@ -6,6 +6,31 @@ class AvatarPage extends StatelessWidget {
     return Scaffold(
       appBar: AppBar(
         title: Text("Avatar Page"),
+        actions: <Widget>[
+          Container(
+            padding: EdgeInsets.all(3),
+            child: CircleAvatar(
+              backgroundImage: NetworkImage(
+                  "https://www.nationalgeographic.com.es/medio/2021/03/09/perro_4da5a8be_800x1200.jpg"),
+              radius: 20,
+            ),
+          ),
+          Container(
+            margin: EdgeInsets.only(right: 10),
+            child: CircleAvatar(
+              child: Text("SL"),
+              backgroundColor: Colors.amberAccent,
+            ),
+          )
+        ],
+      ),
+      body: Center(
+        child: FadeInImage(
+          image: NetworkImage(
+              "https://www.nationalgeographic.com.es/medio/2021/03/09/perro_4da5a8be_800x1200.jpg"),
+          placeholder: AssetImage("assets/jar-loading.gif"),
+          fadeInDuration: Duration(seconds: 2),
+        ),
       ),
     );
   }
